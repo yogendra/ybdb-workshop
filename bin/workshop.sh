85650b0edb8034ed69e6e0a2623f852b21098fce
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-18 02:47:12 +0800

    Add docker build function

--- bin/workshop.sh
+++ bin/workshop.sh
@@ -11,6 +11,7 @@ function update-gitpod-yml(){
     branch-file-update main "$DEST_BRANCH/.gitpod.yml" "$DEST_BRANCH" ".gitpod.yml"
   done
 }
+
 function update-dockerfile(){
   branch-file-update-inplace .ybdb.Dockerfile
 }
@@ -29,7 +30,17 @@ YugabyteDB Workshop Intstructor Utility
 $SCRIPT <COMMANDS> [parameters...]
 
 COMAMNDS
-  workshop-update-gitpod-yml - helps in updating workshop in the branches
+  workshop-update-gitpod-yml    - helps in updating workshop in the branches
+
+  gitpod-workspace-image-build  - build docker image for gitpod workspace
+                                  parameters:
+                                    gitpod-workspace-image-build <yb-version> [repo...]
+                                  example:
+                                    gitpod-workspace-image-build 2.20.0.0 docker.io/my-user
+
+                                    Image name: docker.io/my-user/ybdb-workshop-workspace:2.20.0.0-b76
+
+
 EOF
 }
 function ws-branches-recreate(){
@@ -91,9 +102,28 @@ function branch-dir-update(){
     echo "[$SRC_BRANCH]:$SRC_DIR -> [$DEST_BRANCH]:$DEST_DIR: Skipped - No change"
   fi
 }
+function gitpod-workspace-image-build(){
+  version=${1:-2.20.0.0};
+
+  [[ $# -gt 0 ]] && shift
+  release=$(curl -s https://registry.hub.docker.com/v2/repositories/yugabytedb/yugabyte/tags?name$version |  grep -Eo "${version}-b[0-9]+")
+  image_tag=$release
+  export tags=""
+  if [[ $# -gt 0 ]]; then
+    export tags=$(echo $* | tr ' ' '\n' | sed "s/^/ -t /;s/$/:$image_tag/"|tr -d '\n')
+      # tags="$tags -t $repo/ybdb-workshop-gitpod-workspace:$image_tag"
+      # echo "$tags"
+    # done
+  else
+    export tags="-t yogendra/ybdb-workshop-gitpod-workspace:$image_tag"
+  fi
+
+  docker buildx build --platform linux/x86_64 --build-arg YB_RELEASE=$release -f .ybdb.Dockerfile $tags --push $PROJECT_DIR
+  echo Images created: $tags
+}
 
 OP=${1:-_help}
-if [[ $# -gt 1 ]] ; then shift ; fi
+[[ $# -gt 0 ]] && shift
 
 $OP "$@"
 
