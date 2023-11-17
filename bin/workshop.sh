362d772c50ccac2df58a0a4f2fbbb3896bb1bd21
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-17 22:40:56 +0800

    add common function for inplace file update

--- bin/workshop.sh
+++ bin/workshop.sh
@@ -5,7 +5,6 @@ PROJECT_DIR=$(cd $SCRIPT_DIR/..; pwd)
 SCRIPT=$0
 WS_BRANCHES=$(git branch | grep ws- | tr -d ' ')
 function workshop-update-gitpod-yml(){
-    set +x
     update-branch-file main init-dsql/.gitpod-dsql.yml ws-dsql .gitpod.yml
     update-branch-file main init-cdc/.gitpod-cdc.yml ws-cdc .gitpod.yml
     update-branch-file main init-ft/.gitpod-ft.yml ws-ft .gitpod.yml
@@ -15,14 +14,15 @@ function workshop-update-gitpod-yml(){
     update-branch-file main init-voyager/.gitpod-voyager.yml ws-voyager .gitpod.yml
 }
 function workshop-update-dockerfile(){
-    set +x
-    update-branch-file main .ybdb.Dockerfile ws-dsql .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-cdc .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-ft .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-iloop .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-qt .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-scale .ybdb.Dockerfile
-    update-branch-file main .ybdb.Dockerfile ws-voyager .ybdb.Dockerfile
+  workshop-update-file-inplace .ybdb.Dockerfile
+}
+
+function workshop-update-file-inplace(){
+    file=$1
+    echo $WS_BRANCHES | while read DEST_BRANCH
+    do
+      echo update-branch-file main "$file" "$DEST_BRANCH" "$file"
+    done
 }
 
 function _help(){
@@ -52,6 +52,8 @@ function update-branch-file(){
   if [[ $(git status --porcelain) ]]; then
     git add $SRC_FILE $DEST_FILE
     git commit -m "$DESC"
+  else
+    echo "$SRC_BRANCH/$SRC_FILE -> $DEST_BRANCH/$DEST_FILE: Skipped - No change"
   fi
   git checkout $cbranch
 }
