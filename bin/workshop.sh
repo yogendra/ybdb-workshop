1d5d39c6863cbe6b38f5ad18a2e5e9d0c9b5bd35
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-17 22:57:57 +0800

    update management script

--- bin/workshop.sh
+++ bin/workshop.sh
@@ -4,20 +4,20 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$(cd $SCRIPT_DIR/..; pwd)
 SCRIPT=$0
 WS_BRANCHES="$(git branch | grep ws- | tr -d ' ')"
-function workshop-update-gitpod-yml(){
-    update-branch-file main init-dsql/.gitpod-dsql.yml ws-dsql .gitpod.yml
-    update-branch-file main init-cdc/.gitpod-cdc.yml ws-cdc .gitpod.yml
-    update-branch-file main init-ft/.gitpod-ft.yml ws-ft .gitpod.yml
-    update-branch-file main init-iloop/.gitpod-iloop.yml ws-iloop .gitpod.yml
-    update-branch-file main init-qt/.gitpod-qt.yml ws-qt .gitpod.yml
-    update-branch-file main init-scale/.gitpod-scale.yml ws-scale .gitpod.yml
-    update-branch-file main init-voyager/.gitpod-voyager.yml ws-voyager .gitpod.yml
+function update-gitpod-yml(){
+  branch-file-update main init-dsql/.gitpod-dsql.yml ws-dsql .gitpod.yml
+  branch-file-update main init-cdc/.gitpod-cdc.yml ws-cdc .gitpod.yml
+  branch-file-update main init-ft/.gitpod-ft.yml ws-ft .gitpod.yml
+  branch-file-update main init-iloop/.gitpod-iloop.yml ws-iloop .gitpod.yml
+  branch-file-update main init-qt/.gitpod-qt.yml ws-qt .gitpod.yml
+  branch-file-update main init-scale/.gitpod-scale.yml ws-scale .gitpod.yml
+  branch-file-update main init-voyager/.gitpod-voyager.yml ws-voyager .gitpod.yml
 }
-function workshop-update-dockerfile(){
-  workshop-update-file-inplace .ybdb.Dockerfile
+function update-dockerfile(){
+  branch-file-update-inplace .ybdb.Dockerfile
 }
 
-function workshop-update-file-inplace(){
+function branch-file-update-inplace(){
     file=$1
     echo "$WS_BRANCHES" | while read DEST_BRANCH
     do
@@ -36,7 +36,7 @@ COMAMNDS
 EOF
 }
 
-function update-branch-file(){
+function branch-file-update(){
   SRC_BRANCH=$1;shift
   SRC_FILE=$1; shift
   DEST_BRANCH=$1;shift
@@ -56,6 +56,25 @@ function update-branch-file(){
     echo "$SRC_BRANCH/$SRC_FILE -> $DEST_BRANCH/$DEST_FILE: Skipped - No change"
   fi
   git checkout $cbranch
+}
+
+function branch-dir-update(){
+  SRC_BRANCH=$1;shift
+  SRC_DIR=$1;shift
+  DEST_BRANCH=$1;shift
+  DESC="syncing $SRC_BRANCH/$SRC_DIR to $DEST_BRANCH/$SRC_DIR"
+  cbranch=$(git rev-parse --abbrev-ref HEAD)
+
+  [[ -d $SRC_DIR ]] || (print $SRC_DIR: not a directory; exit 1)
+
+  git checkout $DEST_BRANCH
+  git checkout $SRC_BRANCH $SRC_DIR
+  if [[ $(git status --porcelain) ]]; then
+    git add $SRC_DIR
+    git commit -m "$DESC"
+  else
+    echo "$SRC_BRANCH/$SRC_DIR -> $DEST_BRANCH/$DEST_DIR: Skipped - No change"
+  fi
 }
 
 OP=${1:-_help}
